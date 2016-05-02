b19aa6dd0cdaef8d782e6e4232fa889024c151e1
AlejandroVentimilla <alejoveintimilla@gmail.com>
2016-05-02 12:35:45 -0500

    img videos

--- criptolibertad/settings.py
+++ criptolibertad/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['CRIPTOLIBERTAD_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 HEROKU = True
 
 ALLOWED_HOSTS = ["*"]
