c71092903de2c2120e3e4e75a99c2741b562c8a0
AlejandroVentimilla <alejoveintimilla@gmail.com>
2016-07-08 23:59:55 -0500

    debug false

--- criptolibertad/settings.py
+++ criptolibertad/settings.py
@@ -29,7 +29,7 @@ ADMINS = [('alejo', 'alejoveintimilla@gmail.com')]
 SECRET_KEY = os.environ['CRIPTOLIBERTAD_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 HEROKU = True
 
 ALLOWED_HOSTS = ["*"]
